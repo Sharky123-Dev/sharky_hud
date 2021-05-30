93c9c7eb68aa82151c57cc4f7cf19e3e864ccfd9
SharkyDev <75483199+Sharky123-Dev@users.noreply.github.com>
2021-05-30 01:05:35 -0400

    Create config.lua

---
+++ configs/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- Config Hud
+Config.CommandConfigHud = "confighud"
+
+Config.CommandConfigHudMover = "moverhud"
+
+Config.ActualizacionHud = 2000 -- [milisegundos] Recuerda que esto podria afectar al rendimiento del script.
+
+Config.CommandToggleHud = "togglehud"
